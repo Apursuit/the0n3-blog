f806e4278c2e924632cebbb12cbab3289bfee4ef
Apursuit <131228600+Apursuit@users.noreply.github.com>
2026-03-24 16:59:42 +0800

    add github action,stop tracking dist

--- dist/assets/prism/prism-line-number.js
+++
@@ -1,4 +0,0 @@
-// 放在 site.js 顶部即可（不必等 DOMContentLoaded）
-document.querySelectorAll('pre > code[class*="language-"]').forEach(code => {
-  code.parentElement.classList.add('line-numbers');
-});
--- dist/images/better-shell/demo.js
+++
@@ -1,5 +0,0 @@
-AsciinemaPlayer.create('/medias/better-shell/demo.cast', document.getElementById('demo'), {
-cols: 60,
-rows: 28,
-fit: 'width',  
-});
