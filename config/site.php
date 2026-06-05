4ecfecf766f99bd29071643b0d8b3ade16c9a9e7
Apursuit <131228600+Apursuit@users.noreply.github.com>
2026-06-05 22:33:05 +0800

    docs: update

--- config/site.php
+++ config/site.php
@@ -4,16 +4,17 @@ return [
     'title' => 'the0n3',
     'author' => 'the0n3',
     # Sitemap 使用此域名作为站点根地址
-    'url' => 'https://blog.the0n3.top',
+    'url' => 'https://the0n3.top',
     'description' => 'the0n3',
-    'canonical' => 'https://blog.the0n3.top',
-    'og_image' => 'https://blog.the0n3.top/images/og-default.png',
+    'canonical' => 'https://the0n3.top',
+    'og_image' => 'https://the0n3.top/images/og-default.png',
     'og_locale' => 'zh_CN',
     'nav' => [
         ['label' => '首页', 'url' => '/', 'order' => 10],
         ['label' => '归档', 'url' => '/archives/', 'order' => 20],
         ['label' => '分类', 'url' => '/categories/', 'order' => 30],
         ['label' => '标签', 'url' => '/tags/', 'order' => 40],
+        ['label' => '开往', 'url' => 'https://www.travellings.cn/go.html', 'order' => 70],
     ],
     // Giscus 评论系统（默认关闭，需填写自己的配置）
     'giscus' => [
