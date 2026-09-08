436b9abd2ba725d7491eac38ba0b765199c9eb5d
Apursuit <131228600+Apursuit@users.noreply.github.com>
2026-09-08 18:34:06 +0800

    style: update the number of displays per page

--- src/PageGenerator.php
+++ src/PageGenerator.php
@@ -91,7 +91,8 @@ class PageGenerator
 
     private function generateIndexPages(array $posts, array $siteData, array $navItems, array &$sitemapEntries, string $buildTimestamp, string $siteUrl): void
     {
-        $pageSize = 15;
+        # 每页显示的文章数量
+        $pageSize = 12;
         $pinnedPosts = array_filter($posts, function ($post) {
             return ($post['frontMatter']['pin'] ?? 0) > 0;
         });
